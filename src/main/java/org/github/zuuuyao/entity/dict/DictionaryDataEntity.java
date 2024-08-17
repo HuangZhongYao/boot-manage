d6486937e088aae11298ea26f910566386a4c097
黄中瑶 <HuangZhongYao>
2024-08-18 04:03:39 +0800

    feat: (系统字典): 定义字典实体、mapper

---
+++ src/main/java/org/github/zuuuyao/entity/dict/DictionaryDataEntity.java
@@ -0,0 +1,63 @@
+package org.github.zuuuyao.entity.dict;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serial;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.github.zuuuyao.common.base.entity.AbstractBaseEntity;
+
+/**
+ * 字典数据实体
+ * @Desc Created by IntelliJ IDEA.
+ * @Author ZhongYao.Huang (https://github.com/HuangZhongYao)
+ * @Copyright ZuuuuYao By Github
+ * @Time 2024-08-18 3:42
+ */
+@Setter
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@TableName("sys_dict_data")
+public class DictionaryDataEntity extends AbstractBaseEntity {
+
+    @Serial
+    private static final long serialVersionUID = -4909560354235113011L;
+
+    /**
+     * 上级 DictionaryTypeEntity 主键id
+     */
+    private String parentId;
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 编码
+     */
+    private String code;
+
+    /**
+     * 排序值
+     */
+    private Integer sort;
+
+    /**
+     * 启用状态
+     */
+    private Boolean enable;
+
+    /**
+     * 备注
+     */
+    private String remark;
+}
---
+++ src/main/java/org/github/zuuuyao/repository/DictionaryDataRepository.java
@@ -0,0 +1,15 @@
+package org.github.zuuuyao.repository;
+
+import org.github.zuuuyao.config.mybatis.extension.BaseMapperExtension;
+import org.github.zuuuyao.entity.dict.DictionaryDataEntity;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Desc Created by IntelliJ IDEA.
+ * @Author ZhongYao.Huang (https://github.com/HuangZhongYao)
+ * @Copyright ZuuuuYao By Github
+ * @Time 2024-08-18 3:59
+ */
+@Repository
+public interface DictionaryDataRepository extends BaseMapperExtension<DictionaryDataEntity> {
+}
