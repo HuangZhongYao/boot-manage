76e5315697ac042a12ea33639218aab4bac3837e
黄中瑶 <17685306043@163.com>
2025-07-25 15:40:25 +0800

    feat(config): 为 Swagger 添加全局 Authorization 认证
    - 在 Swagger 配置中添加了名为 "Authorization" 的安全方案
    - 方案类型为 API 密钥，位于 HTTP 头部，用于 JWT Bearer Token 认证
    - 实现了全局 OpenAPI 自定义器，为所有操作添加 Authorization

--- src/main/java/org/github/zuuuyao/config/swagger/SwaggerConfiguration.java
+++ src/main/java/org/github/zuuuyao/config/swagger/SwaggerConfiguration.java
@@ -2,10 +2,16 @@ package org.github.zuuuyao.config.swagger;
 
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 /**
  * swagger配置类
@@ -14,11 +20,10 @@ import org.springframework.context.annotation.Configuration;
  * @Time 2024-07-11 16:23
  * @Author HuangZhongYao
  */
-
+@io.swagger.v3.oas.annotations.security.SecurityScheme(name = "Authorization", type = SecuritySchemeType.APIKEY, in = SecuritySchemeIn.HEADER, description = "JWT Bearer Token 认证", scheme = "token")
 @OpenAPIDefinition(
         info = @Info(
                 title = "Boot-Manage 后台管理系统单体架构Boot版 API文档",
-                version = "1.0.0",
                 description = "Boot-Manage后台管理系统中 所涉及的API文档的详细描述",
                 termsOfService = "http://example.com/terms",
                 license = @License(
@@ -30,5 +35,24 @@ import org.springframework.context.annotation.Configuration;
 )
 @Configuration
 public class SwaggerConfiguration {
+
+    /**
+     * 全局自定义扩展
+     */
+    @Bean
+    public GlobalOpenApiCustomizer globalOpenApiCustomizer() {
+        return openApi -> {
+            // 全局添加Authorization
+            if (openApi.getPaths() != null) {
+                openApi.getPaths().forEach((path, pathItem) -> {
+                    // 其他接口统一添加Authorization
+                    pathItem.readOperations()
+                            .forEach(operation ->
+                                    operation.addSecurityItem(new SecurityRequirement().addList(HttpHeaders.AUTHORIZATION))
+                            );
+                });
+            }
+        };
+    }
 
 }
